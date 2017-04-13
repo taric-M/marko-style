530ce1bce954309646a7af5ed40a16154fda0e1a
Taras <taras92.m@gmail.com>
2017-04-13 22:12:35 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.link-menu').click( function(){ 
+    $('.link').click( function(){ 
 	var scroll_el = $(this).attr('href');
         if ($(scroll_el).length != 0) { 
 	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000); 
